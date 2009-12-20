cd04afa56ebb6e5f9c1edb5b07ec79d25699847c
mobin <=>
2009-12-21 03:05:09 +0330

    commit

---
+++ api/utils.py
@@ -0,0 +1,22 @@
+import re
+from types import FunctionType
+
+
+class Class:
+    dunder_finder = re.compile(r"^__\w+__$")
+
+    def __init__(self, class_: object):
+        self.class_ = class_
+        self.class_contents = class_.__dict__
+        print("class contents:", self.class_contents)
+
+    def extract_attr_names(self):
+        attrs = []
+        class_contents = self.class_contents
+        class_contents_copy = class_contents.copy()
+        for key in class_contents:
+            if isinstance(class_contents_copy[key], FunctionType) or self.dunder_finder.match(key):
+                continue
+            attrs.append(key)
+        return attrs
+        
