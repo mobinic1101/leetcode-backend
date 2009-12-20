4a9fdafae06f4eb7fd4b984738aa93ab4a236a84
mobin <=>
2009-12-21 01:45:34 +0330

    complete sign_up functionality (create auth.py)

---
+++ api/auth.py
@@ -0,0 +1,42 @@
+from rest_framework.permissions import AllowAny
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
+
+from .models import CustomUser
+
+
+def exists(model, val: dict):
+	return True if model.objects.filter(**val).exists() else False
+
+
+@api_view(["POST"])
+@permission_classes(AllowAny)
+def sign_up(request):
+	data = request.data
+	username = data.get("username")
+	password = data.get("password")
+
+	# check if username is taken
+	if exists(CustomUser, {"username":username}):
+		return Response(
+			{"error": "this username is already taken."},
+			status=status.HTTP_400_BAD_REQUEST)
+
+	# password validation
+	try:
+		validate_password(password)
+	except ValidationError as e:
+		return Response(
+			{"error": " ".join(e.error_list)},
+			status=status.HTTP_400_BAD_REQUEST)
+
+	user = CustomUser.objects.create_user(
+		username=username, password=password)
+	token = Token.objects.get_or_create(user=user)
+	return Response(
+			{"token": token.key},
+			status=status.HTTP_201_CREATED)
--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
+from . import auth
 
 
 urlpatterns = [
-	path("api-token-auth", obtain_auth_token)
+	path("api-token-auth", obtain_auth_token),
+	path("sign-up", auth.sign_up, name="sign_up")
 ]
