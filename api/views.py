31a7d14405b95296d039c3eab467e409f81bc9ac
mobin <abdulmobinkarimi451@gmail.com>
2025-01-05 07:14:05 +0330

    commit the remaining untrack/modified files

--- api/views.py
+++ api/views.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.decorators import (
     api_view,
     permission_classes,
-    authentication_classes,
+    # authentication_classes,
 )
 from rest_framework.response import Response
 from rest_framework import generics, status
