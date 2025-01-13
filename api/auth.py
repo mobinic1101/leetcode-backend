d8d6add76abdd32a09879a3e6cd6cd1911e5438b
mobin <abdulmobinkarimi451@gmail.com>
2025-01-14 00:10:23 +0330

    read previous commit message.

--- api/auth.py
+++ api/auth.py
@@ -32,7 +32,7 @@ def sign_up(request):
         validate_password(password)
     except ValidationError as e:
         return Response(
-            {"error": " ".join(e.error_list)}, status=status.HTTP_400_BAD_REQUEST
+            {"detail": " ".join(e.error_list)}, status=status.HTTP_400_BAD_REQUEST
         )
 
     user = CustomUser.objects.create_user(username=username, password=password)
