b5215b5aa25bfd4e4b39909e554abab4362c53d6
mobin <abdulmobinkarimi451@gmail.com>
2025-01-15 23:30:33 +0330

    remove AddIsAuthenticatedToResponse middleware

--- LETTCODE_BACKEND/settings.py
+++ LETTCODE_BACKEND/settings.py
@@ -53,7 +53,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "api.middleware.custom_middleware.AddIsAuthenticatedToResponse"
 ]
 
 REST_FRAMEWORK = {
--- LETTCODE_BACKEND/urls.py
+++ LETTCODE_BACKEND/urls.py
@@ -12,5 +12,3 @@ urlpatterns = [
 print('DEBUG: ', settings.DEBUG)
 if settings.DEBUG: 
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-print(urlpatterns)
